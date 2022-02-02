9a5ee4fec7125757f4ecbff612d6059d7ab45226
Rakibul Hashan pHero <rakibulhashan1603@gmail.com>
2022-02-03 04:19:30 +0600

    final commit

---
+++ for-loop.js
@@ -0,0 +1,24 @@
+// for(var i = 0; i < 7; i++){
+//     // console.log('Roast den, please');
+//     console.log(i)
+// }
+
+// even numbers 
+// for( i= 0; i <= 20; i++){
+//     if(i % 2 == 0 ){
+//         console.log(i);
+//     }
+// }
+
+// odd numbers
+// for(i= 1; i <= 20; i += 2){
+//     console.log(i);
+// }
+
+
+// for loop diya shob element dekhano
+var deskElement = ['mobile', 'keyboard', 'mouse', 'pen', 'paper'];
+
+for (let i = 0; i< deskElement.length; i++){
+    console.log(deskElement[i]);
+}
